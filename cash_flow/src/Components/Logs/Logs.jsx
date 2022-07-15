c883362114530ae22dd673bfbe486ee66969e487
Hygor Bueno <hygor_knd.x1@hotmail.com>
2022-07-15 08:20:12 -0300

    Criando logo, importando fontes e ajustando rotas

--- cash_flow/src/Components/Logs/Logs.jsx
+++ cash_flow/src/Components/Logs/Logs.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-// export default function Logs(props){
-//     return(
-//         <h1>Você está no Logs...</h1>
-//     )
-// }
-
 export default function Logs(props){
     return(
         <h1>Você está em relatórios...</h1>
